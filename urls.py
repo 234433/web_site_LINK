eee0e3a6dfada8156a7f93593660b79870c1bd03
駒田隆人 <takato_2005@icloud.com>
2022-02-12 23:54:48 +0900

    4ばんのcommit

--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+from os import link
 from django.urls import path
 
 from .views import IndexView, AboutView
@@ -6,3 +7,5 @@ urlpatterns = [
     path('', IndexView.as_view()),
     path('about/', AboutView.as_view()),
 ]
+app_name = 'LINK'
+urlpatterns = 
